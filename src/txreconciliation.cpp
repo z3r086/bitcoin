dc47082be410f468f58b45152e600ac2132f9647
Gleb Naumenko <naumenko.gs@gmail.com>
2021-02-25 14:41:11 +0200

    Add helper to compute sketches for tx reconciliation

--- src/txreconciliation.cpp
+++ src/txreconciliation.cpp
@@ -4,12 +4,27 @@
 
 #include <txreconciliation.h>
 
+#include <minisketch/include/minisketch.h>
+
 namespace {
 
 /** Current protocol version */
 constexpr uint32_t RECON_VERSION = 1;
 /** Static component of the salt used to compute short txids for inclusion in sketches. */
 const std::string RECON_STATIC_SALT = "Tx Relay Salting";
+/** The size of the field, used to compute sketches to reconcile transactions (see BIP-330). */
+constexpr unsigned int RECON_FIELD_SIZE = 32;
+/** Limit sketch capacity to avoid DoS. */
+constexpr uint16_t MAX_SKETCH_CAPACITY = 2 << 12;
+/**
+* It is possible that if sketch encodes more elements than the capacity, or
+* if it is constructed of random bytes, sketch decoding may "succeed",
+* but the result will be nonsense (false-positive decoding).
+* Given this coef, a false positive probability will be of 1 in 2**coef.
+*/
+constexpr unsigned int RECON_FALSE_POSITIVE_COEF = 16;
+static_assert(RECON_FALSE_POSITIVE_COEF <= 256,
+    "Reducing reconciliation false positives beyond 1 in 2**256 is not supported");
 /** Default value for the coefficient used to estimate reconciliation set differences. */
 constexpr double DEFAULT_RECON_Q = 0.02;
 /**
@@ -104,6 +119,19 @@ struct ReconciliationInitByThem {
 
     /** Keep track of the reconciliation phase with the peer. */
     ReconciliationPhase m_phase{RECON_NONE};
+
+    /**
+     * Estimate a capacity of a sketch we will send or use locally (to find set difference)
+     * based on the local set size.
+     */
+    uint16_t EstimateSketchCapacity(size_t local_set_size) const
+    {
+        const uint16_t set_size_diff = std::abs(uint16_t(local_set_size) - m_remote_set_size);
+        const uint16_t min_size = std::min(uint16_t(local_set_size), m_remote_set_size);
+        const uint16_t weighted_min_size = m_remote_q * min_size;
+        const uint16_t estimated_diff = 1 + weighted_min_size + set_size_diff;
+        return minisketch_compute_capacity(RECON_FIELD_SIZE, estimated_diff, RECON_FALSE_POSITIVE_COEF);
+    }
 };
 
 /**
@@ -147,6 +175,14 @@ class ReconciliationState {
      */
     std::set<uint256> m_local_set;
 
+    /**
+     * Reconciliation sketches are computed over short transaction IDs.
+     * This is a cache of these IDs enabling faster lookups of full wtxids,
+     * useful when peer will ask for missing transactions by short IDs
+     * at the end of a reconciliation round.
+     */
+    std::map<uint32_t, uint256> m_local_short_id_mapping;
+
     /** Keep track of reconciliations with the peer. */
     ReconciliationInitByUs m_state_init_by_us;
     ReconciliationInitByThem m_state_init_by_them;
@@ -164,6 +200,34 @@ class ReconciliationState {
         const uint64_t s = SipHashUint256(m_k0, m_k1, wtxid);
         const uint32_t short_txid = 1 + (s & 0xFFFFFFFF);
         return short_txid;
+    }
+
+    /**
+     * Reconciliation involves computing a space-efficient representation of transaction identifiers
+     * (a sketch). A sketch has a capacity meaning it allows reconciling at most a certain number
+     * of elements (see BIP-330).
+     */
+    Minisketch ComputeSketch(uint16_t capacity)
+    {
+        Minisketch sketch;
+        // Avoid serializing/sending an empty sketch.
+        if (m_local_set.size() == 0 || capacity == 0) return sketch;
+
+        std::vector<uint32_t> short_ids;
+        for (const auto& wtxid: m_local_set) {
+            uint32_t short_txid = ComputeShortID(wtxid);
+            short_ids.push_back(short_txid);
+            m_local_short_id_mapping.emplace(short_txid, wtxid);
+        }
+
+        capacity = std::min(capacity, MAX_SKETCH_CAPACITY);
+        sketch = Minisketch(RECON_FIELD_SIZE, 0, capacity);
+        if (sketch) {
+            for (const uint32_t short_id: short_ids) {
+                sketch.Add(short_id);
+            }
+        }
+        return sketch;
     }
 };
 
