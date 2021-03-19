a9275d7410aa2902f0eed54f9233bf7bb29cd53d
Gleb Naumenko <naumenko.gs@gmail.com>
2021-03-19 14:00:27 +0200

    Finish negotiating reconciliation support
    Once we received a reconciliation announcement support
    message from a peer and it doesn't violate our protocol,
    we store the negotiated parameters which will be used
    for future reconciliations.

--- src/txreconciliation.cpp
+++ src/txreconciliation.cpp
@@ -24,6 +24,44 @@ uint256 ComputeSalt(uint64_t local_salt, uint64_t remote_salt)
     return (CHashWriter(RECON_SALT_HASHER) << salt1 << salt2).GetSHA256();
 }
 
+/**
+ * Used to keep track of the ongoing reconciliations, the transactions we want to announce to the
+ * peer when next transaction reconciliation happens, and also all parameters required to perform
+ * reconciliations.
+ */
+class ReconciliationState {
+    friend TxReconciliationTracker;
+
+    /**
+     * Reconciliation protocol assumes using one role consistently: either a reconciliation
+     * initiator (requesting sketches), or responder (sending sketches). This defines our role.
+     * */
+    const bool m_we_initiate;
+
+    /**
+     * We flood specific transactions to some of the peers we reconcile with to enable faster
+     * transaction relay while still conserving bandwidth by reconciling in most of the cases.
+     * More specifically, we flood to a limited number of outbound reconciling peers
+     * *for which this flag is enabled* (and also to non-reconciling peers, although this is
+     * irrelevant here).
+     * This flag is enabled based on whether we have a sufficient number of outbound transaction
+     * relay peers already.
+     * Transactions announced via flooding should not be added to the reconciliation set.
+     */
+    const bool m_flood_to;
+
+    /**
+     * Reconciliation involves exchanging sketches, which efficiently represent transactions each
+     * peer wants to announce. Sketches are computed over transaction short IDs.
+     * These values are used to salt short IDs.
+     */
+    const uint64_t m_k0, m_k1;
+
+    ReconciliationState(bool we_initiate, bool flood_to, uint64_t k0, uint64_t k1) :
+        m_we_initiate(we_initiate), m_flood_to(flood_to),
+        m_k0(k0), m_k1(k1) {}
+};
+
 } // namespace
 
 /** Actual implementation for TxReconciliationTracker's data structure. */
@@ -39,6 +77,11 @@ class TxReconciliationTracker::Impl {
      * - halting of relay of particular transactions due to short ID collisions (DoS)
      */
     std::unordered_map<NodeId, uint64_t> m_local_salts GUARDED_BY(m_mutex);
+
+    /**
+     * Keeps track of ongoing reconciliations with a given peer.
+     */
+    std::unordered_map<NodeId, ReconciliationState> m_states GUARDED_BY(m_mutex);
 
     public:
 
@@ -62,11 +105,49 @@ class TxReconciliationTracker::Impl {
         return std::make_tuple(we_initiate_recon, we_respond_recon, RECON_VERSION, m_local_recon_salt);
     }
 
+    void EnableReconciliationSupport(NodeId peer_id, bool inbound,
+        bool they_may_initiate, bool they_may_respond, uint32_t recon_version, uint64_t remote_salt)
+    {
+        // We do not support reconciliation salt/version updates, so receiving this message
+        // for the second time should not happen
+        LOCK(m_mutex);
+        auto recon_state = m_states.find(peer_id);
+        if (recon_state != m_states.end()) return;
+
+        recon_version = std::min(recon_version, RECON_VERSION);
+        if (recon_version < 1) return;
+
+        // Must match SuggestReconciliation logic.
+        bool we_may_initiate = !inbound, we_may_respond = inbound;
+
+        bool they_initiate = they_may_initiate && we_may_respond;
+        bool we_initiate = we_may_initiate && they_may_respond;
+        // If we ever announce we_initiate && we_may_respond, this will need tie-breaking. For now,
+        // this is mutually exclusive because both are based on the inbound flag.
+        assert(!(they_initiate && we_initiate));
+
+        if (!(they_initiate || we_initiate)) return;
+
+        // To save bandwidth, we never flood to inbound peers we reconcile with. We may flood *some*
+        // transactions to outbound peers we reconcile with.
+        // TODO: Flood only through a limited number of outbound connections.
+        bool flood_to = !inbound;
+
+        LogPrint(BCLog::NET, "Register peer=%d for reconciliation with the following params: "
+            "we_initiate=%i, they_initiate=%i, flood_to=%i\n", peer_id, we_initiate, they_initiate, flood_to);
+
+        uint256 full_salt = ComputeSalt(m_local_salts.at(peer_id), remote_salt);
+
+        m_states.emplace(peer_id, ReconciliationState(we_initiate, flood_to,
+            full_salt.GetUint64(0), full_salt.GetUint64(1)));
+    }
+
     void RemovePeer(NodeId peer_id)
     {
         LogPrint(BCLog::NET, "Stop tracking reconciliation state for peer=%d\n", peer_id);
         LOCK(m_mutex);
         m_local_salts.erase(peer_id);
+        m_states.erase(peer_id);
     }
 
 };
@@ -79,6 +160,13 @@ TxReconciliationTracker::~TxReconciliationTracker() = default;
 std::tuple<bool, bool, uint32_t, uint64_t> TxReconciliationTracker::SuggestReconciling(NodeId peer_id, bool inbound)
 {
     return m_impl->SuggestReconciling(peer_id, inbound);
+}
+
+void TxReconciliationTracker::EnableReconciliationSupport(NodeId peer_id, bool inbound,
+    bool recon_requestor, bool recon_responder, uint32_t recon_version, uint64_t remote_salt)
+{
+    m_impl->EnableReconciliationSupport(peer_id, inbound, recon_requestor, recon_responder,
+        recon_version, remote_salt);
 }
 
 void TxReconciliationTracker::RemovePeer(NodeId peer_id)
