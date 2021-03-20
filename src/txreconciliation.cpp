ce4e80c885bf4aed668d1d3878fe576fb6c6d1fa
Gleb Naumenko <naumenko.gs@gmail.com>
2021-03-20 12:26:41 +0200

    Add helper to see if a peer reconciles txs with us

--- src/txreconciliation.cpp
+++ src/txreconciliation.cpp
@@ -150,6 +150,11 @@ class TxReconciliationTracker::Impl {
         m_states.erase(peer_id);
     }
 
+    bool IsPeerRegistered(NodeId peer_id) const
+    {
+        LOCK(m_mutex);
+        return m_states.find(peer_id) != m_states.end();
+    }
 };
 
 TxReconciliationTracker::TxReconciliationTracker() :
@@ -172,4 +177,9 @@ void TxReconciliationTracker::EnableReconciliationSupport(NodeId peer_id, bool i
 void TxReconciliationTracker::RemovePeer(NodeId peer_id)
 {
     m_impl->RemovePeer(peer_id);
+}
+
+bool TxReconciliationTracker::IsPeerRegistered(NodeId peer_id) const
+{
+    return m_impl->IsPeerRegistered(peer_id);
 }
