8eee36657b504eef9768fd597b8151f8272e349b
Gleb Naumenko <naumenko.gs@gmail.com>
2021-03-23 20:41:09 +0200

    Be ready to receive sketch extension
    Store the initial sketches so that we are able to process
    extension sketch while avoiding transmitting the same data.

--- src/protocol.cpp
+++ src/protocol.cpp
@@ -48,7 +48,7 @@ const char *SENDTXRCNCL="sendtxrcncl";
 const char *REQTXRCNCL="reqtxrcncl";
 const char* SKETCH = "sketch";
 const char* RECONCILDIFF = "reconcildiff";
-const char *REQSKETCHEXT="reqsketchext";
+const char* REQSKETCHEXT = "reqsketchext";
 } // namespace NetMsgType
 
 /** All known message types. Keep this in the same order as the list of
