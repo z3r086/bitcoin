ba3abfd25f30159636400ee3e6c42bb872e7d91a
Gleb Naumenko <naumenko.gs@gmail.com>
2021-03-23 12:26:17 +0200

    p2p: Track reconciliation requests schedule
    We initiate reconciliation by looking at the queue periodically
    with equal intervals between peers to achieve efficiency.
    
    This will be later used to see whether it's time to initiate.

--- src/node/txreconciliation.cpp
+++ src/node/txreconciliation.cpp
@@ -32,6 +32,15 @@ constexpr double OUTBOUND_FANOUT_DESTINATIONS = 1;
 static_assert(OUTBOUND_FANOUT_DESTINATIONS >= 1);
 
 /**
+ * Interval between initiating reconciliations with peers.
+ * This value allows to reconcile ~(7 tx/s * 8s) transactions during normal operation.
+ * More frequent reconciliations would cause significant constant bandwidth overhead
+ * due to reconciliation metadata (sketch sizes etc.), which would nullify the efficiency.
+ * Less frequent reconciliations would introduce high transaction relay latency.
+ */
+constexpr std::chrono::microseconds RECON_REQUEST_INTERVAL{8s};
+
+/**
  * Salt (specified by BIP-330) constructed from contributions from both peers. It is used
  * to compute transaction short IDs, which are then used to construct a sketch representing a set
  * of transactions we want to announce to the peer.
@@ -109,6 +118,25 @@ private:
      * easier to estimate set difference size.
      */
     std::deque<NodeId> m_queue GUARDED_BY(m_txreconciliation_mutex);
+
+    /**
+     * Make reconciliation requests periodically to make reconciliations efficient.
+     */
+    std::chrono::microseconds m_next_recon_request GUARDED_BY(m_txreconciliation_mutex){0};
+    void UpdateNextReconRequest(std::chrono::microseconds now) EXCLUSIVE_LOCKS_REQUIRED(m_txreconciliation_mutex)
+    {
+        // We have one timer for the entire queue. This is safe because we initiate reconciliations
+        // with outbound connections, which are unlikely to game this timer in a serious way.
+        size_t we_initiate_to_count = std::count_if(m_states.begin(), m_states.end(),
+                                                    [](std::pair<NodeId, std::variant<uint64_t, TxReconciliationState>> indexed_state) {
+                                                        const auto* cur_state = std::get_if<TxReconciliationState>(&indexed_state.second);
+                                                        if (cur_state) return cur_state->m_we_initiate;
+                                                        return false;
+                                                    });
+
+        Assert(we_initiate_to_count != 0);
+        m_next_recon_request = now + (RECON_REQUEST_INTERVAL / we_initiate_to_count);
+    }
 
 public:
     explicit Impl(uint32_t recon_version) : m_recon_version(recon_version) {}
